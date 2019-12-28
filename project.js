44adbb193b8a94ffd5fe87542d74c3291c5cd3e3
cutroiramkeo <57948074+cutroiramkeo@users.noreply.github.com>
2019-12-28 21:58:38 +0700

    Add files via upload

---
+++ project.js
@@ -0,0 +1,16 @@
+function bxh(){
+    return{
+        'Position': '',
+        'Club': '',
+        'Played': '',
+        'Won': '',
+        'Drawn': '',
+        'Lost': '',
+        'GF': '',
+        'GA': '',
+        'GD': '',
+        'Points': '',
+    }
+}
+
+let
