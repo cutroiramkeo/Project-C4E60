b647ae70df2535365b61a5fa29b750692a8b50b0
cutroiramkeo <57948074+cutroiramkeo@users.noreply.github.com>
2020-01-13 22:07:01 +0700

    Add files via upload

---
+++ players.js
@@ -0,0 +1,209 @@
+function getDefaultClubs() {
+    return{
+    'Name': "",
+    'Position': "",
+    'Image': "",
+    'Link': "",
+    'Club': "",
+};
+}
+
+let clubs = [];
+
+let clubs1 = getDefaultClubs();
+clubs1.Name = "Riyad Mahrez";
+clubs1.Club = "Manchester City";
+clubs1.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p103025.png";
+clubs1.Link = "https://www.premierleague.com/players/8983/Riyad-Mahrez/overview";
+clubs1.Position = "Forward";
+clubs.push(clubs1);
+
+let clubs2 = getDefaultClubs();
+clubs2.Name = "Mohamed Salah";
+clubs2.Club = "Liverpool";
+clubs2.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p118748.png";
+clubs2.Link = "https://www.premierleague.com/players/5178/Mohamed-Salah/overview";
+clubs2.Position = "Forward";
+clubs.push(clubs2);
+
+let clubs3 = getDefaultClubs();
+clubs3.Name = "Marcus Rashford";
+clubs3.Club = "Manchester United";
+clubs3.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p176297.png";
+clubs3.Link = "https://www.premierleague.com/players/13565/Marcus-Rashford/overview";
+clubs3.Position = "Forward";
+clubs.push(clubs3);
+
+let clubs4 = getDefaultClubs();
+clubs4.Name = "Jamie Vardy";
+clubs4.Club = "Leicester City";
+clubs4.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p101668.png";
+clubs4.Link = "https://www.premierleague.com/players/8979/Jamie-Vardy/overview";
+clubs4.Position = "Forward";
+clubs.push(clubs4);
+
+let clubs5 = getDefaultClubs();
+clubs5.Name = "Kevin de Bruyne";
+clubs5.Club = "Manchester City";
+clubs5.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p61366.png";
+clubs5.Link = "https://www.premierleague.com/players/4288/Kevin-De-Bruyne/overview";
+clubs5.Position = "Midfielder";
+clubs.push(clubs5);
+
+let clubs6 = getDefaultClubs();
+clubs6.Name = "Paul Pogba";
+clubs6.Club = "Manchester United";
+clubs6.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p74208.png";
+clubs6.Link = "https://www.premierleague.com/players/3920/Paul-Pogba/overview";
+clubs6.Position = "Midfielder";
+clubs.push(clubs6);
+
+let clubs7 = getDefaultClubs();
+clubs7.Name = "Jordan Henderson";
+clubs7.Club = "Liverpool";
+clubs7.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p56979.png";
+clubs7.Link = "https://www.premierleague.com/players/3712/Jordan-Henderson/overview";
+clubs7.Position = "Midfielder";
+clubs.push(clubs7);
+
+let clubs8 = getDefaultClubs();
+clubs8.Name = "James Maddison";
+clubs8.Club = "Leicester City";
+clubs8.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p172780.png";
+clubs8.Link = "https://www.premierleague.com/players/8456/James-Maddison/overview";
+clubs8.Position = "Midfielder";
+clubs.push(clubs8);
+
+let clubs9 = getDefaultClubs();
+clubs9.Name = "Virgil van Dijk";
+clubs9.Club = "Liverpool";
+clubs9.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p97032.png";
+clubs9.Link = "https://www.premierleague.com/players/5140/Virgil-van-Dijk/overview";
+clubs9.Position = "Defender";
+clubs.push(clubs9);
+
+let clubs10 = getDefaultClubs();
+clubs10.Name = "Toby Alderweireld";
+clubs10.Club = "Tottenham Hotspur";
+clubs10.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p55605.png";
+clubs10.Link = "https://www.premierleague.com/players/4916/Toby-Alderweireld/overview";
+clubs10.Position = "Defender";
+clubs.push(clubs10);
+
+let clubs11 = getDefaultClubs();
+clubs11.Name = "César Azpilicueta";
+clubs11.Club = "Chelsea";
+clubs11.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p41328.png";
+clubs11.Link = "https://www.premierleague.com/players/4496/C%C3%A9sar-Azpilicueta/overview";
+clubs11.Position = "Defender";
+clubs.push(clubs11);
+
+let clubs12 = getDefaultClubs();
+clubs12.Name = "Archie Davies";
+clubs12.Club = "Brighton and Hove Albion";
+clubs12.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p215061.png";
+clubs12.Link = "https://www.premierleague.com/players/14555/Archie-Davies/overview";
+clubs12.Position = "Defender";
+clubs.push(clubs12);
+
+let clubs13 = getDefaultClubs();
+clubs13.Name = "Alisson Becker";
+clubs13.Club = "Liverpool";
+clubs13.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p116535.png";
+clubs13.Link = "https://www.premierleague.com/players/20559/Alisson/overview";
+clubs13.Position = "Goalkeeper";
+clubs.push(clubs13);
+
+let clubs14 = getDefaultClubs();
+clubs14.Name = "Kepa Arrizabalaga";
+clubs14.Club = "Chelsea";
+clubs14.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p109745.png";
+clubs14.Link = "https://www.premierleague.com/players/11373/Kepa-Arrizabalaga/overview";
+clubs14.Position = "Goalkeeper";
+clubs.push(clubs14);
+
+let clubs15 = getDefaultClubs();
+clubs15.Name = "Kasper Schmeichel";
+clubs15.Club = "Leicester City";
+clubs15.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p17745.png";
+clubs15.Link = "https://www.premierleague.com/players/2508/Kasper-Schmeichel/overview";
+clubs15.Position = "Goalkeeper";
+clubs.push(clubs15);
+
+let clubs16 = getDefaultClubs();
+clubs16.Name = "David de Gea";
+clubs16.Club = "Manchester United";
+clubs16.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p51940.png";
+clubs16.Link = "https://www.premierleague.com/players/4330/David-de-Gea/overview";
+clubs16.Position = "Goalkeeper";
+clubs.push(clubs16);
+
+let clubs17 = getDefaultClubs();
+clubs17.Name = "Tammy Abraham";
+clubs17.Club = "Chelsea";
+clubs17.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p173879.png";
+clubs17.Link = "https://www.premierleague.com/players/13286/Tammy-Abraham/overview";
+clubs17.Position = "Forward";
+clubs.push(clubs17);
+
+let clubs18 = getDefaultClubs();
+clubs18.Name = "Dele Alli";
+clubs18.Club = "Tottenham Hotspur";
+clubs18.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p108823.png";
+clubs18.Link = "https://www.premierleague.com/players/9167/Dele-Alli/overview";
+clubs18.Position = "Midfielder";
+clubs.push(clubs18);
+
+let clubs19 = getDefaultClubs();
+clubs19.Name = "Trent Alexander-Arnold";
+clubs19.Club = "Liverpool";
+clubs19.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p169187.png";
+clubs19.Link = "https://www.premierleague.com/players/14732/Trent-Alexander-Arnold/overview";
+clubs19.Position = "Defender";
+clubs.push(clubs19);
+
+let clubs20 = getDefaultClubs();
+clubs20.Name = "Ederson";
+clubs20.Club = "Manchester City";
+clubs20.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p121160.png";
+clubs20.Link = "https://www.premierleague.com/players/12707/Ederson/overview";
+clubs20.Position = "Goalkeeper";
+clubs.push(clubs20);
+
+
+
+
+
+
+
+
+function displaylistclubs(listclubs) {
+
+
+
+let body = document.getElementById("list-clubs");
+ let s = '';
+ for (let i = 0; i < listclubs.length; i++) {
+     const clubs = listclubs[i];
+     let tr = 
+     `<div class="col md-4"><div class="card"> 
+     <img class="card-img-top" height ="240px" src="${clubs.Image}" alt="Card image">
+     <div class="card-body" >
+         <h4 >${clubs.Name}</h4>
+         <p >${clubs.Position}</p>
+         <p >${clubs.Club}</p>
+         <a  href="${clubs.Link}" target="blank" class="btn btn-primary"> View
+                        Profile</a>
+     </div>
+     </div> 
+     </div>`;
+     s += tr;
+
+
+ }
+
+
+ body.innerHTML = s;
+
+}
+displaylistclubs(clubs);
