ccdeae8aebd6168774e15ccf7a48c8bdd4015cf2
ankhang1710 <57948083+ankhang1710@users.noreply.github.com>
2020-01-17 22:14:06 +0700

    Add files via upload

---
+++ admin1.js
@@ -0,0 +1,326 @@
+function getDefaultPlayers() {
+    return {
+      'Name': '',
+      'Image': '',
+      'Club': '',
+      'Position': '',
+      'Description': '',
+      'Link': '',
+      
+      
+    };
+  }
+  
+  let Players = [];
+  let players1 = getDefaultPlayers();
+  players1.Name = "Riyad Mahrez";
+  players1.Club = "Manchester City";
+  players1.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p103025.png";
+  players1.Link = "https://www.premierleague.com/players/8983/Riyad-Mahrez/overview";
+  players1.Position = "Forward";
+  Players.push(players1);
+  
+  let players2 = getDefaultPlayers();
+  players2.Name = "Mohamed Salah";
+  players2.Club = "Liverpool";
+  players2.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p118748.png";
+  players2.Link = "https://www.premierleague.com/players/5178/Mohamed-Salah/overview";
+  players2.Position = "Forward";
+  Players.push(players2);
+  
+  let players3 = getDefaultPlayers();
+  players3.Name = "Marcus Rashford";
+  players3.Club = "Manchester United";
+  players3.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p176297.png";
+  players3.Link = "https://www.premierleague.com/players/13565/Marcus-Rashford/overview";
+  players3.Position = "Forward";
+  Players.push(players3);
+  
+  let players4 = getDefaultPlayers();
+  players4.Name = "Jamie Vardy";
+  players4.Club = "Leicester City";
+  players4.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p101668.png";
+  players4.Link = "https://www.premierleague.com/players/8979/Jamie-Vardy/overview";
+  players4.Position = "Forward";
+  Players.push(players4);
+  
+  let players5 = getDefaultPlayers();
+  players5.Name = "Kevin de Bruyne";
+  players5.Club = "Manchester City";
+  players5.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p61366.png";
+  players5.Link = "https://www.premierleague.com/players/4288/Kevin-De-Bruyne/overview";
+  players5.Position = "Midfielder";
+  Players.push(players5);
+  
+  let players6 = getDefaultPlayers();
+  players6.Name = "Paul Pogba";
+  players6.Club = "Manchester United";
+  players6.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p74208.png";
+  players6.Link = "https://www.premierleague.com/players/3920/Paul-Pogba/overview";
+  players6.Position = "Midfielder";
+  Players.push(players6);
+  
+  let Players7 = getDefaultPlayers();
+  Players7.Name = "Jordan Henderson";
+  Players7.Club = "Liverpool";
+  Players7.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p56979.png";
+  Players7.Link = "https://www.premierleague.com/players/3712/Jordan-Henderson/overview";
+  Players7.Position = "Midfielder";
+  Players.push(Players7);
+  
+  let players8 = getDefaultPlayers();
+  players8.Name = "James Maddison";
+  players8.Club = "Leicester City";
+  players8.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p172780.png";
+  players8.Link = "https://www.premierleague.com/players/8456/James-Maddison/overview";
+  players8.Position = "Midfielder";
+  Players.push(players8);
+  
+  let players9 = getDefaultPlayers();
+  players9.Name = "Virgil van Dijk";
+  players9.Club = "Liverpool";
+  players9.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p97032.png";
+  players9.Link = "https://www.premierleague.com/players/5140/Virgil-van-Dijk/overview";
+  players9.Position = "Defender";
+  Players.push(players9);
+  
+  let players10 = getDefaultPlayers();
+  players10.Name = "Toby Alderweireld";
+  players10.Club = "Tottenham Hotspur";
+  players10.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p55605.png";
+  players10.Link = "https://www.premierleague.com/players/4916/Toby-Alderweireld/overview";
+  players10.Position = "Defender";
+  Players.push(players10);
+  
+  let players11 = getDefaultPlayers();
+  players11.Name = "César Azpilicueta";
+  players11.Club = "Chelsea";
+  players11.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p41328.png";
+  players11.Link = "https://www.premierleague.com/players/4496/C%C3%A9sar-Azpilicueta/overview";
+  players11.Position = "Defender";
+  Players.push(players11);
+  
+  let players12 = getDefaultPlayers();
+  players12.Name = "Archie Davies";
+  players12.Club = "Brighton and Hove Albion";
+  players12.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p215061.png";
+  players12.Link = "https://www.premierleague.com/players/14555/Archie-Davies/overview";
+  players12.Position = "Defender";
+  Players.push(players12);
+  
+  let players13 = getDefaultPlayers();
+  players13.Name = "Alisson Becker";
+  players13.Club = "Liverpool";
+  players13.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p116535.png";
+  players13.Link = "https://www.premierleague.com/players/20559/Alisson/overview";
+  players13.Position = "Goalkeeper";
+  Players.push(players13);
+  
+  let players14 = getDefaultPlayers();
+  players14.Name = "Kepa Arrizabalaga";
+  players14.Club = "Chelsea";
+  players14.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p109745.png";
+  players14.Link = "https://www.premierleague.com/players/11373/Kepa-Arrizabalaga/overview";
+  players14.Position = "Goalkeeper";
+  Players.push(players14);
+  
+  let players15 = getDefaultPlayers();
+  players15.Name = "Kasper Schmeichel";
+  players15.Club = "Leicester City";
+  players15.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p17745.png";
+  players15.Link = "https://www.premierleague.com/players/2508/Kasper-Schmeichel/overview";
+  players15.Position = "Goalkeeper";
+  Players.push(players15);
+  
+  let players16 = getDefaultPlayers();
+  players16.Name = "David de Gea";
+  players16.Club = "Manchester United";
+  players16.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p51940.png";
+  players16.Link = "https://www.premierleague.com/players/4330/David-de-Gea/overview";
+  players16.Position = "Goalkeeper";
+  Players.push(players16);
+  
+  let players17 = getDefaultPlayers();
+  players17.Name = "Tammy Abraham";
+  players17.Club = "Chelsea";
+  players17.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p173879.png";
+  players17.Link = "https://www.premierleague.com/players/13286/Tammy-Abraham/overview";
+  players17.Position = "Forward";
+  Players.push(players17);
+  
+  let Players18 = getDefaultPlayers();
+  Players18.Name = "Dele Alli";
+  Players18.Club = "Tottenham Hotspur";
+  Players18.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p108823.png";
+  Players18.Link = "https://www.premierleague.com/players/9167/Dele-Alli/overview";
+  Players18.Position = "Midfielder";
+  Players.push(Players18);
+  
+  let players19 = getDefaultPlayers();
+  players19.Name = "Trent Alexander-Arnold";
+  players19.Club = "Liverpool";
+  players19.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p169187.png";
+  players19.Link = "https://www.premierleague.com/players/14732/Trent-Alexander-Arnold/overview";
+  players19.Position = "Defender";
+  Players.push(players19);
+  
+  let players20 = getDefaultPlayers();
+  players20.Name = "Ederson";
+  players20.Club = "Manchester City";
+  players20.Image = "https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/p121160.png";
+  players20.Link = "https://www.premierleague.com/players/12707/Ederson/overview";
+  players20.Position = "Goalkeeper";
+  Players.push(players20);
+  
+  
+  
+  
+  let checkExist = localStorage.getItem("AllPlayers");
+  if (checkExist === null) {
+    localStorage.setItem('AllPlayers', JSON.stringify(Players));
+  } else {
+    Players = JSON.parse(checkExist);
+  }
+  function displayListPlayers(listPlayers) {
+    let body = document.getElementById("list-players");
+
+    let s = '';
+    for (let i = 0; i < listPlayers.length; i++) {
+      const players = listPlayers[i];
+      let tr = `
+        <tr>
+          <td>${i + 1}</td>
+          <td>${players.Name}</td>
+          <td><img width="50px" src="${players.Image}" alt ="đây là ảnh" ></td>
+          <td>${players.Club}</td>
+          <td>${players.Position}</td>
+          <td>${players.description}</td>
+          <td>
+            <button class="btn btn-success" onclick="editPlayers('${players.Name}')">Edit</button>
+            <button class="btn btn-warning" onclick="deletePlayers('${players.Name}')">Delete</button>
+          </td>
+        </tr>`;
+      s += tr;
+    }
+    body.innerHTML = s;
+  }
+  displayListPlayers(Players);//
+  
+  function editPlayers(Name) {
+    let currentPlayers;
+    for (let i = 0; i < Players.length; i++) {
+      const players = Players[i];
+      if (players.Name === Name) {
+        currentPlayers = players;
+
+        break;
+      }
+    }
+  
+    $('#editPlayers').modal('show');
+    document.getElementById('txteditName').value = currentPlayers.Name;
+    document.getElementById('txteditClubs').value = currentPlayers.Club;
+    document.getElementById('txteditPosition').value = currentPlayers.Position;
+    document.getElementById('txteditImage').value = currentPlayers.Image;
+    document.getElementById('txteditDescription').value = currentPlayers.Description;
+    document.getElementById('txteditLink').value = currentPlayers.Link;
+  }
+  
+  
+  function deletePlayers(Name) {
+    if (confirm("Bạn có muốn xóa không ?")) {
+      for (let i = 0; i < Players.length; i++) {
+        const players = Players[i];
+        if (players.Name === Name) {
+          Players.splice(i, 1);
+          break;
+        }
+      }
+      displayListPlayers(Players);
+      localStorage.setItem('AllPlayers', JSON.stringify(Players));
+    }
+  }
+  
+  function addPlayers(e) {
+    e.preventDefault();
+  
+    let Name = document.getElementById('txtName').value;
+    let Club = document.getElementById('txtClubs').value;
+    let Position = document.getElementById('txtPosition').value;
+    let Image = document.getElementById('txtImage').value;
+    let Description = document.getElementById('txtDes').value;
+    let Link = document.getElementById('txtLink').value;
+    let newPlayers = getDefaultPlayers();
+    newPlayers.Name = Name;
+    newPlayers.Club = Club;
+    newPlayers.Position = Position;
+    newPlayers.Image = Image;
+    newPlayers.Description = Description;
+    newPlayers.Link = Link;
+
+
+    Players.push(newPlayers);
+  
+    displayListPlayers(Players);
+  
+    document.getElementById('frmnewPlayers').reset();
+    $('#addPlayers').modal('hide');
+    localStorage.setItem('AllPlayers', JSON.stringify(Players));
+  }
+  
+  function savePlayers(e) {
+    e.preventDefault();
+  
+    let Club = document.getElementById('txteditClubs').value;
+    let Name = document.getElementById('txteditName').value;
+    let Position = document.getElementById('txteditPosition').value;
+    let Description = document.getElementById('txteditDescription').value;
+    let Image = document.getElementById('txteditImage').value;
+    let Link = document.getElementById('txteditLink').value;
+
+
+
+  
+    for (let i = 0; i < Players.length; i++) {
+      const players = Players[i];
+      if (players.Name === Name) {
+        players.Club = Club;
+        players.Position = Position;
+        players.Image = Image;
+        players.Description = Description;
+        players.Name = Name;
+        players.Link = Link;
+        
+
+        
+        break;
+      }
+    }
+  
+    displayListPlayers(Players);
+  
+    document.getElementById('frmeditPlayers').reset();
+    $('#editPlayers').modal('hide');
+    localStorage.setItem('AllPlayers', JSON.stringify(Players));
+
+  } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+  
+
+
+
